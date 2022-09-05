e3189dfde8b31326339f0b9f665a67e31945a160
Anna-andrade <annaandrade@id.uff.br>
2022-09-05 11:29:50 -0300

    Initial Commit

---
+++ POC-TelaInsta/POC_TelaInstaApp.swift
@@ -0,0 +1,17 @@
+//
+//  POC_TelaInstaApp.swift
+//  POC-TelaInsta
+//
+//  Created by Anna Carolina Costa Andrade on 05/09/22.
+//
+
+import SwiftUI
+
+@main
+struct POC_TelaInstaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
